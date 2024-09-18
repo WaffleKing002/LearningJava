e0b101c72cf7c5e7eba68dfb485646ef2453a9e9
Michael <chinemeremnweke002@gmail.com>
2024-09-18 16:07:23 +0100

    Commit 13: Ternary Operators.

--- JavaClass.java
+++ JavaClass.java
@@ -2,15 +2,14 @@ public class JavaClass {
 
     public static void main(String args[])
     {
-        int x = 77;
-        int y = 18;
-        int z = 22;
-        if(x>y && x>z)
-            System.out.println(x);
-        else if(y>z)
-            System.out.println(y);
+        int x = 70;
+        int result = 0;
+        if(x%2==0)
+            result = 10;
         else
-            System.out.println(z);
+            result = 20;
+          System.out.println(result);
+
     }
 
 }
