c2690da917ba77045fbb3c7daf3beb76f1d89424
Michael <chinemeremnweke002@gmail.com>
2024-09-18 16:13:36 +0100

    Commit 14: Ternary Operators2.

--- JavaClass.java
+++ JavaClass.java
@@ -4,11 +4,8 @@ public class JavaClass {
     {
         int x = 70;
         int result = 0;
-        if(x%2==0)
-            result = 10;
-        else
-            result = 20;
-          System.out.println(result);
+        result = x%2==0 ? 10 : 20;
+            System.out.println(result);
 
     }
 
