89f4b6e33893772b8281cc83033a9985a5836231
Michael <chinemeremnweke002@gmail.com>
2024-09-18 10:21:42 +0100

    Commit 3: Variables

--- JavaClass.java
+++ JavaClass.java
@@ -1,5 +1,16 @@
 public class JavaClass {
-    public static void main(String args[]){
-        System.out.println("Welcome ");
+
+    public static void main(String args[])
+    {
+
+           int num1 = 15;
+           int num2 = 45;
+           int num3 = 3;
+           int result = num1 * num2 / num3;
+           System.out.println(result);
+
+
+
     }
+
 }
