023f02b7952f586f9371c188819ae82a2acb12af
Michael <chinemeremnweke002@gmail.com>
2024-10-09 11:46:43 +0100

    For loops

---
+++ ForLoops.java
@@ -0,0 +1,42 @@
+public class ForLoops
+{
+
+    public static void main(String args[])
+    {
+
+
+        for(int i=1;i<=7;i++)
+        {
+            System.out.println("Day " + i);
+
+            for(int j=1;j<=9;j++)
+            {
+                System.out.println("   " + (j+8) + "-" + (j+9));
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
