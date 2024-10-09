0528937a964e1beacd3890e75cdd0d95062cece3
Michael <chinemeremnweke002@gmail.com>
2024-10-09 10:01:56 +0100

    Using Loops in Java

---
+++ UsingLoops.java
@@ -0,0 +1,23 @@
+public class UsingLoops {
+
+
+    public static void main(String args[]) {
+        int i = 1;
+
+        while (i <= 4) {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello " + j);
+                j++;
+            }
+
+            i++;
+
+
+        }
+
+        System.out.println("Bye " + i);
+    }
+
+}
